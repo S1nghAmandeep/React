7066121a4d87b7a7d8358ed615d3bb1ff1a86551
Amandeep <amandeep.singh@farnetworks.com>
2024-11-28 15:45:15 +0100

    laidation lesson

---
+++ form-lesson/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Login', e.target.email.value, e.target.password.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
